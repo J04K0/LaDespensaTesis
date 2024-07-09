ea991e639021b118c9e796156afcde28a85d747d
J04K0KID <joaquin.perez2102@alumnos.ubiobio.cl>
2024-07-08 20:45:56 -0400

    Creacion login

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,24 +1,16 @@
-import LoginForm from '../components/LoginForm';
-import { useNavigate } from 'react-router-dom';
+import axios from './root.service';
+import jwtDecode from 'jwt-decode';
 
-function Login() {
-  const navigate = useNavigate();
-
-  if (localStorage.getItem('user')) {
-    return (
-      <>
-        <h2>Ya estas logeado!</h2>
-        <button onClick={() => navigate('/')}>Ir a home</button>
-      </>
-    );
+export const login = async ({ email, password }) => {
+  try {
+    const response = await axios.post('/auth/login', { email, password });
+    const { status, data } = response;
+    if (status === 200) {
+      const { email, roles } = jwtDecode(data.data.accessToken);
+      localStorage.setItem('user', JSON.stringify({ email, roles }));
+      axios.defaults.headers.common['Authorization'] = `Bearer ${data.data.accessToken}`;
+    }
+  } catch (error) {
+    console.error('Login error:', error);
   }
-
-  return (
-    <div>
-      <h2>Inicia sesion!</h2>
-      <LoginForm />
-    </div>
-  );
-}
-
-export default Login;
+};
